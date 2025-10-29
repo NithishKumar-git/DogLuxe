14c74efde3561e8f8af4676a1a0f0e39dcded54f
unknown <snithishk7@gmail.com>
2025-10-29 16:02:47 +0530

    my new ecommerce site

---
+++ javascript/collections.js
@@ -0,0 +1,38 @@
+var collect1 = document.getElementById("collect1");
+var search = document.getElementById("search");
+var collect1item = collect1.querySelectorAll(".collections-cont");
+
+search.addEventListener("keyup", function(event) {
+    var enteredvalue = event.target.value.toUpperCase();
+
+    for (var count = 0; count < collect1item.length; count++) {
+        var collect1name = collect1item[count].querySelector("h3").textContent.toUpperCase();
+
+        if (collect1name.indexOf(enteredvalue) < 0) {
+            collect1item[count].style.display = "none";
+        } else {
+            collect1item[count].style.display = "block";
+        }
+    }
+});
+var side_bar = document.querySelector(".side-bar")
+
+var menu_tog = document.querySelector(".menu-tog")
+
+function shownav(){
+   side_bar.style.left ="0"
+
+}
+
+function hidenav(){
+    side_bar.style.left="-60%"
+}
+function mt() {
+  let title = document.querySelector(".collections-cont h3");
+  title.style.opacity = "1";
+}
+
+
+function current(){
+  alert("Currently Not Available")
+}
---
+++ javascript/js.js
@@ -0,0 +1,23 @@
+var side_bar = document.querySelector(".side-bar")
+
+var menu_tog = document.querySelector(".menu-tog")
+
+function shownav(){
+   side_bar.style.left ="0"
+
+}
+
+function hidenav(){
+    side_bar.style.left="-70%"
+}
+function mt() {
+  let title = document.querySelector(".collections-cont h3");
+  title.style.opacity = "1";
+}
+
+
+function current(){
+  alert("Currently Not Available")
+}
+
+
